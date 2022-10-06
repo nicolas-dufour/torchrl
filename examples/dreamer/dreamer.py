0c597d6962452e806e7448680b34f95b19cb44ca
Nicolas Dufour <ndufour@fb.com>
2022-10-06 11:18:11 +0100

    Torchdynamo initial commit

--- examples/dreamer/dreamer.py
+++ examples/dreamer/dreamer.py
@@ -200,7 +200,7 @@ def main(cfg: "DictConfig"):  # noqa: F821
         logger = WandbLogger(
             f"dreamer/{exp_name}",
             project="torchrl",
-            group=f"Dreamer_{cfg.env_name}",
+            group=f"Dreamer_{cfg.env_name}_dynamo",
             offline=cfg.offline_logging,
         )
     elif cfg.logger == "csv":
